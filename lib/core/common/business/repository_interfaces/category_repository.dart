bbbce8c9bce9c42c8ae3877e7754c12007ce7aa0
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-05-05 13:54:33 +0200

    fixed file name `category_entity`

--- lib/core/common/business/repository_interfaces/category_repository.dart
+++ lib/core/common/business/repository_interfaces/category_repository.dart
@@ -1,5 +1,5 @@
 import '../../../params/category_params.dart';
-import '../../domain/entities/category_entitty.dart';
+import '../../domain/entities/category_entity.dart';
 
 abstract class CategoryRepository {
   Future<CategoryEntity> getCategory({required CategoryParams params});
