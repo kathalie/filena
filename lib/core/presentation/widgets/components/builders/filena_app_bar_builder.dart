41ea3bc116138000c003f9c38764534a147fab8a
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2025-05-21 22:14:25 +0200

    fixed import

--- lib/core/presentation/widgets/components/builders/filena_app_bar_builder.dart
+++ lib/core/presentation/widgets/components/builders/filena_app_bar_builder.dart
@@ -4,10 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
+import '../../../../../features/files_management/presentation/widgets/buttons/upload_file_button_widget.dart';
 import '../app_logo.dart';
 import '../buttons/notifications_button.dart';
 import '../buttons/prompts_history_button.dart';
-import '../../../../../features/files_management/presentation/views/buttons/upload_file_button_widget.dart';
 
 PlatformAppBar buildFilenaAppBar() {
   return PlatformAppBar(
